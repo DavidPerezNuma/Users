1c65fb0f9e0842d799c5f537069756f020cb314c
David Perez Numa <juperez49@unisalle.edu.co>
2026-03-15 09:10:19 -0500

    Feat: Implement request validation middleware
    - Create validator.handler.js for schema validation
    - Integrate Joi for request data validation
    - Add support for validating body, params, and query parameters
    - Fix double next() call to prevent routing issues

---
+++ server/middlewares/validator.handler.js
@@ -0,0 +1,15 @@
+import Boom from "@hapi/boom";
+
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate(data, { abortEarly: false }); 
+    
+    if (error) {
+      return next(Boom.badRequest(error));
+    }
+    next();
+  }
+}
+
+export { validatorHandler };
